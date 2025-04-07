940060de1475091a274b1bb92d7ddc2d9e187c6e
devkiraa <devkiraa@gmail.com>
2025-04-07 07:47:43 +0530

    Update to-do list and simplify MongoDB connection: add a page to view all orders live and streamline MongoDB connection code.

--- server.js
+++ server.js
@@ -52,12 +52,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
 }));
-
 // Connect to MongoDB Atlas using MONGO_URI from .env.
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Set EJS as templating engine.
